9d3089ed037d0bec632d772597f6aa08b8f88b74
Emily <emily.jiang@fairlinkcentury.com>
2018-04-02 16:02:09 +0800

    [python] mooc 上例子

---
+++ python/simple/circles.py
@@ -0,0 +1,15 @@
+# windows
+import turtle
+
+turtle.setup(width=400,height=400, startx=100, starty=100)
+turtle.penup()
+turtle.goto(0, -100)
+
+turtle.pendown()
+turtle.pensize(2)
+turtle.circle(10)
+turtle.circle(40)
+turtle.circle(80)
+turtle.circle(160)
+
+turtle.mainloop()
